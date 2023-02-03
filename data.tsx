fa1120925e76e1900adcad52ea9a2349c7b89ac5
yashasviPearlOrg <yashasvi@pearlorganisation.com>
2023-02-03 17:12:31 +0530

    New Images Added

---
+++ data.tsx
@@ -0,0 +1,25 @@
+const data = [
+    {
+      title: "OnBoarding Screen 1",
+      body: "Stay close with family in one touch, we can help you.",
+      imgUrl: "./assets/img1.png",
+    },
+    {
+        title: "OnBoarding Screen 2",
+        body: "With us you can be calm to your friends and parents.",
+        imgUrl: "./assets/img2.png",
+    },
+    {
+        title: "OnBoarding Screen 3",
+        body: "We will help you in your business aspects.",
+        imgUrl: "./assets/img3.png",
+    },
+    {
+        title: "OnBoarding Screen 4",
+        body: "We will also help you with vehicle tracking.",
+        imgUrl: "./assets/img4.png",
+    },
+
+  ];
+  
+  export default data;
